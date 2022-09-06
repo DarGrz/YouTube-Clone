e079f8fbc9901a808b61d8b7b9a81c0efecb9d65
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-09-06 15:07:09 +0200

    components/VideoDetail: created

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typograpy, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Video } from "./";
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 function VideoDetail() {
   return <div>VideoDetail</div>;
